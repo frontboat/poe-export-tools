6dcc60f5da3b652f352858adbf0780e4c50fd0fc
frontboat <156139764+frontboat@users.noreply.github.com>
2025-12-26 23:11:46 -0700

    also set target to bun

--- fullstack/build.ts
+++ fullstack/build.ts
@@ -1,5 +1,6 @@
 await Bun.build({
   entrypoints: ["./server.ts"],
+  target: "bun",
   compile: {
     target: "bun-linux-x64", //change this to bun-darwin-arm64-modern for deploying on a mac in local dev. check out bun --compile docs 
     execArgv: ["--smol"],
